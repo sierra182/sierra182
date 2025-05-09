602278a2e5b5073e0e517cccfb4ab1882361d751
sierra182 <sierra-echo@msn.com>
2025-05-09 16:53:33 +0200

    mdf myscript

--- myscripts/mygit.sh
+++ myscripts/mygit.sh
@@ -3,7 +3,7 @@
 echo -ne "\033[93;1m\n  Enter commit message: "
 read commit_message
 command="git add . && git commit -m \"$commit_message\" && git push && git status"
-echo -e "\033[33;1m  Command to execute: \033[36;1m$command\033[0m"
+echo -e "\033[33;1m  Command to execute: \033[31;1m$command\033[0m"
 echo -ne "\033[93;1m  Execute this command? (y/n): "
 read execute
 if [[ $execute == "y" ]]; then 
