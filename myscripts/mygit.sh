6a86ae38655ac00991ab843c5f2bb4a948d55849
sierra182 <sierra-echo@msn.com>
2025-05-09 16:19:24 +0200

    upd myscripts

--- myscripts/mygit.sh
+++ myscripts/mygit.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-read -p "Enter commit message: " commit_message
+echo -ne "\033[93;1m\n  Enter commit message: "
+read commit_message
 command="git add . && git commit -m \"$commit_message\" && git push && git status"
-echo "Command to execute: $command"
-read -p "Execute this command? (y/n): " execute
+echo -e "\033[33;1m  Command to execute: \033[36;1m$command\033[0m"
+echo -ne "\033[93;1m  Execute this command? (y/n): "
+read execute
 if [[ $execute == "y" ]]; then 
+    echo -e "\033[32m"
     eval "$command"
 else
-    echo "Command execution aborted."
+    echo -e "\033[31;1m  Command execution aborted.\033[0m"
 fi
+echo -ne "\n\033[0m"
